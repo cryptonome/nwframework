3673a0c1819c880b75c683e9c7bc3645973de3b1
cryptonome <cryptonome@99739cec-fd51-0410-a8a5-87d694e196b2>
2008-07-14 21:10:04 +0000

    License headers missed, solved

--- trunk/Framework/Utils/NWCriticalSection.h
+++ trunk/Framework/Utils/NWCriticalSection.h
@@ -1,14 +1,21 @@
-/* 
- * Copyright 2007-2008 InCrew Software, All Rights Reserved
+/*
+ *       This file is part of NWFramework.
+ *       Copyright (c) InCrew Software and Others.
+ *       (See the AUTHORS file in the root of this distribution.)
  *
- * This file may be used or modified without the need for a license.
+ *       NWFramework is free software; you can redistribute it and/or modify
+ *       it under the terms of the GNU General Public License as published by
+ *       the Free Software Foundation; either version 2 of the License, or
+ *       (at your option) any later version.
  *
- * Redistribution of this file in either its original form, or in an
- * updated form may be done under the terms of the GNU LIBRARY GENERAL
- * PUBLIC LICENSE.  If this license is unacceptable to you then you
- * may not redistribute this work.
+ *       NWFramework is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *       GNU General Public License for more details.
  * 
- * See the file COPYING.GPL for details.
+ *       You should have received a copy of the GNU General Public License
+ *       along with NWFramework; if not, write to the Free Software
+ *       Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 #ifndef _INCREW_CRITICAL_SECTION_H_
 #define _INCREW_CRITICAL_SECTION_H_
